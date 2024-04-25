8996afb3f3f437ca5c595732854e3b79a4b0bea3
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-04-25 23:46:44 +0200

    CommentCommit

--- Webprojekt/businesslogic/simpleLogic.php
+++ Webprojekt/businesslogic/simpleLogic.php
@@ -1,22 +1,23 @@
 <?php
 include("db/dataHandler.php");
 
-class SimpleLogic
+class SimpleLogic //SimpleLogic class is defined
 {
     private $dh;
     function __construct()
     {
-        $this->dh = new DataHandler();
+        $this->dh = new DataHandler(); //create Datahandler-Object 
     }
 
     function handleRequest($method)
     {
         switch ($method) {
             case "queryDataFromDatabase":
-                $res = $this->dh->queryDataFromDatabase();
-                break;
+                $res = $this->dh->queryDataFromDatabase(); //if the method is queryDataFromDatabase the 
+                //queryDataFromDatabase from DataHandler is called.
+                break; 
             default:
-                $res = null;
+                $res = null; //if any other function is in $method then null is returned
                 break;
         }
         return $res;
